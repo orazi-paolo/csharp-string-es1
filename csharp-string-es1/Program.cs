3300ba25c4cbe940cf1141c8bf21180026d55821
orazi-paolo <orazi.paolo10@gmail.com>
2024-12-28 16:48:28 +0100

    first commit

---
+++ csharp-string-es1/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        string esempio = "Questa è una stringa classica \nQuesta è una stringa con una nuova riga";
+        Console.WriteLine(esempio);
+
+
+        string percorso = """
+        C:\Documenti\Immagini
+        """;
+        Console.WriteLine(percorso);
+
+    }
+}
